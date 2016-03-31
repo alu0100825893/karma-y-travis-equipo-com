68359f5ac1a0996bfbb29c2a6e37a688bba06c5a
Carlos Troyano Carmona <carlos.troyano.carmona@gmail.com>
2016-03-31 19:20:39 +0000

    añadiendo mas navegadores a karma

--- karma.conf.js
+++ karma.conf.js
@@ -62,7 +62,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome', 'PhantomJS2', 'PhantomJS2_custom'],
+    browsers: ['Firefox', 'Chrome', 'PhantomJS'],
 
 
     // Continuous Integration mode
