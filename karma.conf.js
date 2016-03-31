c2ea9a1ce0533f626370d36d1c70af4efba9fffb
Carlos Troyano Carmona <carlos.troyano.carmona@gmail.com>
2016-03-31 19:37:41 +0000

    probando karma

--- karma.conf.js
+++ karma.conf.js
@@ -23,12 +23,10 @@ module.exports = function(config) {
 
       'tests/*.js'
     ],
-
     // list of files to exclude
     exclude: [
 
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
